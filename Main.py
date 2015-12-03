f9a5a520c6bc6ba7ded7dd8fd6dd538a279daae3
Chris Lucian <Chris@DeadlyApps.com>
2015-12-02 23:41:42 -0800

    Controller Pattern placed in to handle changing screen content and working around tkinter's quit command

--- Main.py
+++ Main.py
@@ -1,13 +1,3 @@
-from tkinter import *
-
-from Infrastructure.MobberManager import MobberManager
-from forms.ScreenBlockerMenu import ScreenBlockerMenu
-from Infrastructure.TimeOptionsManager import TimeOptionsManager
-
-root = Tk()
-
-time_options_manager = TimeOptionsManager()
-mobber_manager = MobberManager()
-screen_blocker_menu = ScreenBlockerMenu(root, time_options_manager, mobber_manager)
-
+from Frames.MobTimerController import MobTimerController
+root = MobTimerController()
 root.mainloop()
