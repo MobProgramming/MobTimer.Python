8782a9aa8ac14d011322bd178eb1f14cce104d87
Chris Lucian <Chris@DeadlyApps.com>
2016-02-26 08:23:20 -0800

    UUID now in topic to allow for different instances on the same session with the same name

--- Infrastructure/DojoManager.py
+++ Infrastructure/DojoManager.py
@@ -36,7 +36,8 @@ class DojoManager(object):
         self.controller = controller
         self.dojo_broker = self.controller.settings_manager.get_dojo_broker()
         self.dojo_port = self.controller.settings_manager.get_dojo_port()
-        self.dojo_mob_station_name = self.controller.settings_manager.get_dojo_mob_station_name() + uuid.uuid4().__str__()
+        self.dojo_station_uuid = uuid.uuid4().__str__()
+        self.dojo_mob_station_name = self.controller.settings_manager.get_dojo_mob_station_name()
         self.dojo_session_id = self.controller.settings_manager.get_dojo_session_id()
         self.dojo_topic_root = self.controller.settings_manager.get_dojo_topic_root()
         self.controller.mobber_manager.subscribe_to_mobber_list_change(self.publish_mobber_list_changes)
@@ -51,12 +52,14 @@ class DojoManager(object):
     def publish_time_change(self, time_string, minutes, seconds, origin_station_name=None):
         topic = self.generate_topic(TIME_CHANGE)
         station_name = self.dojo_mob_station_name
+        station_uuid = self.dojo_station_uuid
         if origin_station_name is not None:
             return
         payload_dictionary = {
             "minutes": minutes,
             "seconds": seconds,
-            "station_name": station_name
+            "station_name": station_name,
+            "station_uuid": station_uuid
         }
         payload = json.dumps(payload_dictionary)
         self.publish(topic, payload)
@@ -88,30 +91,31 @@ class DojoManager(object):
         topic_parts = msg.topic.split('/')
         topic_root = topic_parts[0]
         session_id = topic_parts[1]
-        station_name = topic_parts[2]
-        message_type = topic_parts[3]
-        print("on_message",msg.topic)
-        self.switch_statement_dictionary_trick(station_name, message_type, msg.payload)
+        station_uuid = topic_parts[2]
+        station_name = topic_parts[3]
+        message_type = topic_parts[4]
+        print("on_message",msg.topic,"station_uuid",station_uuid)
+        self.switch_statement_dictionary_trick(station_uuid,station_name, message_type, msg.payload)
 
-    def switch_statement_dictionary_trick(self, station_name, message_type, payload):
-        self.switch_dictionary[message_type](station_name, message_type, payload)
+    def switch_statement_dictionary_trick(self, station_uuid,station_name, message_type, payload):
+        self.switch_dictionary[message_type](station_uuid,station_name, message_type, payload)
 
-    def sub_mobber_list(self, station_name, message_type, payload):
-        if not station_name == self.dojo_mob_station_name:
+    def sub_mobber_list(self,  station_uuid,station_name, message_type, payload):
+        if not station_uuid == self.dojo_station_uuid:
             payload_dictionary = json.loads(payload.decode("utf-8"))
             mobber_list = payload_dictionary["mobber_list"]
             print("sub_mobber_list",mobber_list)
             self.controller.mobber_manager.set_mobber_list(mobber_list)
 
     def generate_topic(self, message_type):
-        topic = "{}/{}/{}/{}".format(self.dojo_topic_root, self.dojo_session_id, self.dojo_mob_station_name,
+        topic = "{}/{}/{}/{}/{}".format(self.dojo_topic_root, self.dojo_session_id,self.dojo_station_uuid, self.dojo_mob_station_name,
                                      message_type)
         print("generate_topic",topic)
         return topic
 
-    def sub_time_change(self, station_name, message_type, payload):
+    def sub_time_change(self, station_uuid, station_name, message_type, payload):
         print(payload)
-        if not station_name == self.dojo_mob_station_name:
+        if not station_uuid == self.dojo_station_uuid:
             payload_dictionary = json.loads(payload.decode("utf-8"))
             minutes = payload_dictionary["minutes"]
             seconds = payload_dictionary["seconds"]
@@ -120,15 +124,15 @@ class DojoManager(object):
                             self.controller.time_options_manager.minutes == minutes and self.controller.time_options_manager.seconds == seconds):
                 self.controller.time_options_manager.set_countdown_time(minutes, seconds, origin_station_name)
 
-    def sub_say_hello(self, station_name, message_type, payload):
-        if not station_name == self.dojo_mob_station_name:
+    def sub_say_hello(self,  station_uuid,station_name, message_type, payload):
+        if not station_uuid == self.dojo_station_uuid:
             if not self.other_stations.__contains__(station_name):
                 self.other_stations.append(station_name)
                 topic = self.generate_topic(SAY_HELLO)
                 self.publish(topic, "")
 
-    def sub_start_timer(self, station_name, message_type, payload):
-        if not station_name == self.dojo_mob_station_name:
+    def sub_start_timer(self, station_uuid, station_name, message_type, payload):
+        if not station_uuid == self.dojo_station_uuid:
             self.controller.launch_transparent_countdown_if_blocking()
 
     def subscribe_to_dojo(self):
